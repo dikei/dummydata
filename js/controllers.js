cc7006dea327ee998fc0f2f8bd0070efe063dade
dikei <duckientruong@gmail.com>
2013-03-21 22:52:41 +0700

    Using Faker.js to generate data

--- js/controllers.js
+++ js/controllers.js
@@ -14,6 +14,8 @@ function InputController($scope) {
         }
     ];
 
+    $scope.datas = {};
+
     $scope.addRow = function() {
         var count = 0;
         while(count < $scope.addMore) {
@@ -27,5 +29,48 @@ function InputController($scope) {
 
     $scope.deleteRow = function(index) {
         $scope.columns.remove(index);
+    }
+
+    var funcMap = {
+        'type-first-name': [Faker.Name.firstName, Faker.Name],
+        'type-last-name': [Faker.Name.lastName, Faker.Name],
+        'type-name': [Faker.Name.findName, Faker.Name],
+        'type-phone': [Faker.PhoneNumber.phoneNumber, Faker.PhoneNumber],
+        'type-zipcode': [Faker.Address.zipCode, Faker.Address],
+        'type-city': [Faker.Address.city, Faker.Address],
+        'type-street-name': [Faker.Address.streetName, Faker.Address],
+        'type-street-address': [Faker.Address.streetAddress, Faker.Address],
+        'type-secondary-address': [Faker.Address.secondaryAddress, Faker.Address],
+        'type-state': [Faker.Address.usState, Faker.Address],
+        'type-county': [Faker.Address.ukCounty, Faker.Address],
+        'type-country': [Faker.Address.ukCountry, Faker.Address],
+        'type-email': [Faker.Internet.email, Faker.Internet],
+        'type-user-name': [Faker.Internet.userName, Faker.Internet],
+        'type-domain-name': [Faker.Internet.domainName, Faker.Internet],
+        'type-domain-word': [Faker.Internet.domainWord, Faker.Internet],
+        'type-ip': [Faker.Internet.ip, Faker.Internet],
+        'type-company-name': [Faker.Company.companyName, Faker.Company],
+        'type-company-suffix': [Faker.Company.companySuffix, Faker.Company],
+        'type-catch-phrase': [Faker.Company.catchPhrase, Faker.Company],
+        'type-paragraph': [Faker.Lorem.paragraph, Faker.Lorem],
+        'type-paragraphs': [Faker.Lorem.paragraphs, Faker.Lorem],
+        'type-sentence': [Faker.Lorem.sentence, Faker.Lorem],
+        'type-sentences': [Faker.Lorem.sentences, Faker.Lorem],
+        'type-number': [Faker.Helpers.randomNumber, Faker.Helpers, [100000]]
+    };
+
+    $scope.generate = function() {
+        var datas = {};
+        for(var i = 0; i < $scope.columns.length; i++) {
+            var column = $scope.columns[i];
+            var func = funcMap[column.type][0];
+            var functhis = funcMap[column.type][1];
+            var funcargs = [];
+            if(funcMap[column.type].length == 3) {
+                funcargs = funcMap[column.type][2]
+            }
+            datas[column.name] = func.apply(functhis, funcargs);
+        }
+        $scope.datas = datas;
     }
 }
