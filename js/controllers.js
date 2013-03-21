2618d9caab69169ede16d4c13176c677b8219fea
dikei <duckientruong@gmail.com>
2013-03-21 20:41:15 +0700

    Implement add row function. Add debug output.

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,20 @@
 function InputController($scope) {
+    $scope.addMore = 1;
     $scope.columns = [
         {
             name: "",
             type: ""
         }
     ];
+
+    $scope.addRow = function() {
+        var count = 0;
+        while(count < $scope.addMore) {
+            ++count;
+            $scope.columns.push({
+                name: "",
+                type: ""
+            });
+        }
+    }
 }
